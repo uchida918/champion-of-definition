f019a95fca983d049eebea12d33d082613e642d5
Makoto Uchida <makoto_uchida918@yahoo.co.jp>
2017-07-31 15:38:06 +0000

    test result

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,28 +1,37 @@
 class TestsController < ApplicationController
   @@test_count = 0
+  @@answers = []
 
   def start
-    @@test_count = 0
     p "start"
     p @@test_count
+    @datas = Definition.all.sample(5)
+    @@test_count = 0
+    @@answers = []
   end
   
   def question
     p "start"
     p @@test_count
+    p @datas = params[:datas]
+    p @data = Definition.find(params[:datas][@@test_count].to_i)
   end
   
   def answer
     @@test_count += 1
     p "start"
     p @@test_count
+    p @datas = params[:datas].split(' ')
+    p @answer = params[:test][:answer]
+    p @@answers << @answer
     if @@test_count >= 5
-      redirect_to tests_result_url
+      redirect_to tests_result_url(datas: @datas)
     else
-      redirect_to tests_question_url
+      redirect_to tests_question_url(datas: @datas)
     end
   end
   
   def result
+    @answers = @@answers
   end
 end

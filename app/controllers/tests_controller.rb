ab6cc896399dd815268705a5e35502055fe7e896
Makoto Uchida <makoto_uchida918@yahoo.co.jp>
2017-08-10 02:30:48 +0000

    Test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -5,17 +5,29 @@ class TestsController < ApplicationController
   def start
     p "start"
     p @@test_count
-    p @datas = Definition.all.sample(5).pluck(:id).join(' ')
+    # definitions = Definition.all
+    # all_datas = definitions.sample(definitions.count)
+    # p @datas = all_datas.pluck(:id).join(' ')
     @@test_count = 0
     @@answers = []
   end
   
   def question
-    p @datas = params[:datas].split(' ')
-    if @@test_count >= 5
+    if params[:type]
+      @type = params[:type]
+      @definitions = Definition.where(category_id: Category.find_by(name: @type))
+      count = @definitions.count
+    else
+      @definitions = Definition.all
+      count = @definitions.count
+    end
+    p @datas = @definitions.pluck(:id)
+    if @@test_count >= count
       return redirect_to tests_result_url(datas: @datas)
     end
     p "question"
+    p "definitions_count #{count}"
+    p params
     p @@test_count
     p @definition = Definition.find(@datas[@@test_count])
       @user_definition = UserDefinition.find_by(definition_id: @datas[@@test_count])
@@ -24,18 +36,20 @@ class TestsController < ApplicationController
   def post_answer
     @@test_count += 1
     p "post_answer"
+    p params[:type]
     p @@test_count
     p @datas = params[:datas].split(' ')
     p @answer = params[:test][:answer]
     p @@answers << @answer
-    redirect_to tests_answer_path(datas: @datas)
+    redirect_to tests_answer_path(datas: @datas, type: params[:type])
   end
   
   def get_answer
+    @type = params[:type]
     p @@test_count
     p "get_answer"
     p params
-    p @datas = params[:datas].split(' ').flatten
+     p @datas = params[:datas]
     p @@answers
     @definition =  Definition.find(@datas[@@test_count - 1])
     @user_definition = UserDefinition.find_by(definition_id: @datas[@@test_count - 1])

9ecff7466d14e049510ad050a262759e68f19f4a
Makoto Uchida <makoto_uchida918@yahoo.co.jp>
2017-08-12 02:01:14 +0000

    fix test params

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,54 +4,53 @@ class TestsController < ApplicationController
 
   def start
     p "start"
-    p @@test_count
-    # definitions = Definition.all
-    # all_datas = definitions.sample(definitions.count)
-    # p @datas = all_datas.pluck(:id).join(' ')
     @@test_count = 0
     @@answers = []
   end
   
   def question
-    if params[:type]
+    p "question"
+    if params[:type].present?
       @type = params[:type]
-      @definitions = Definition.where(category_id: Category.find_by(name: @type))
+      @definitions = Definition.where(category_id: Category.find_by(name: @type)) || Definition.all
       count = @definitions.count
     else
       @definitions = Definition.all
       count = @definitions.count
     end
-    p @datas = @definitions.pluck(:id)
+    @datas = @definitions.pluck(:id) or params[:datas]
     if @@test_count >= count
       return redirect_to tests_result_url(datas: @datas)
     end
-    p "question"
-    p "definitions_count #{count}"
-    p params
-    p @@test_count
-    p @definition = Definition.find(@datas[@@test_count])
-      @user_definition = UserDefinition.find_by(definition_id: @datas[@@test_count])
+
+    @definition = Definition.find(@datas[@@test_count])
+    @user_definition = UserDefinition.find_by(definition_id: @datas[@@test_count])
   end
   
   def post_answer
-    @@test_count += 1
     p "post_answer"
-    p params[:type]
-    p @@test_count
-    p @datas = params[:datas].split(' ')
-    p @answer = params[:test][:answer]
-    p @@answers << @answer
+    @@test_count += 1
+    @datas = params[:datas]
+    @answer = params[:test][:answer]
+    @@answers << @answer
     redirect_to tests_answer_path(datas: @datas, type: params[:type])
   end
   
   def get_answer
-    @type = params[:type]
-    p @@test_count
     p "get_answer"
-    p params
-     p @datas = params[:datas]
-    p @@answers
-    @definition =  Definition.find(@datas[@@test_count - 1])
+    @type = params[:type]
+    if params[:datas].class == String
+      @datas = params[:datas].split(' ')
+    else
+      @datas = params[:datas]
+    end
+
+    if @@test_count == 1
+      @definition =  Definition.find(@datas[0])
+    else
+      @definition =  Definition.find(@datas[@@test_count - 1])
+    end
+    
     @user_definition = UserDefinition.find_by(definition_id: @datas[@@test_count - 1])
     @answer = @@answers[@@test_count - 1]
     render :answer
